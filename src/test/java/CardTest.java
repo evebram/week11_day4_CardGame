3ad8449ef03f1c25d7b6034be370e19557f37e10
= <evebramley@me.com>
2019-09-05 12:37:35 +0100

    CardTest passing

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,10 +1,29 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CardTest {
     Card card;
 
     @Before
     public void before(){
-        this.card = new Card();
+        this.card = new Card(Suit.DIAMONDS, Rank.ACE);
     }
+
+    @Test
+    public void testGetSuit(){
+        assertEquals(Suit.DIAMONDS, card.getSuit());
+    }
+
+    @Test
+    public void testGetRank(){
+        assertEquals(Rank.ACE, card.getRank());
+    }
+
+    @Test
+    public void testGetValue(){
+        assertEquals(1, card.getRankValue());
+    }
+
 }
